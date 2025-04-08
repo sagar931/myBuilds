c4f8272fe27cca944530cacbe57b9e9e4cb3560a
sagar931 <sagarmandal931@gmail.com>
2025-04-08 09:53:00 +0530

    first commit

---
+++ darkmode.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.getElementById('theme-toggle');
+    toggleBtn?.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+    });
+  });
